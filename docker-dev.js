491b6211448a36caca88328e2676dd0c85373d7a
Pedro Câmara <camara-pedro@hotmail.com>
2017-10-20 00:50:59 +0100

    Improved docker-dev script

--- docker-dev.js
+++ docker-dev.js
@@ -2,5 +2,17 @@
 require('dotenv-safe').load({ allowEmptyValues: true });
 const { exec } = require('child_process');
 
-exec(`docker-compose run -d -p ${process.env.DB_PORT || 5432}:5432 postgres`).stdout.pipe(process.stdout);
-exec(`docker-compose run -d -p ${process.env.REDIS_PORT || 5432}:6379 redis`).stdout.pipe(process.stdout);
+const run = function (command, callback) {
+    const stream = exec(command, {}, () => {
+        if (callback) {
+            callback();
+        }
+    });
+    stream.stdout.pipe(process.stdout);
+    stream.stderr.pipe(process.stderr);
+};
+
+run('docker network prune -f', () => {
+    run(`docker-compose run -d -p ${process.env.DB_PORT || 5432}:5432 postgres`);
+    run(`docker-compose run -d -p ${process.env.REDIS_PORT || 5432}:6379 redis`);
+});
